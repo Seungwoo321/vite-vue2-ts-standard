0d93b65b13574768a1bf8b1a8ded9c33630fa2db
Seungwoo321 <seungwoo321@gmail.com>
2025-05-07 20:27:49 +0900

    feat: setup

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'eslint/config'
+import standardJs from '@seungwoo321/eslint-plugin-standard-js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,vue}'],
+    ignores: ['eslint.config.js', "dist/**/*"],
+    extends: [
+      ...tseslint.configs.recommended,
+      ...standardJs.configs.recommended,
+      ...pluginVue.configs['flat/strongly-recommended']
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname
+      }
+    }
+  }
+])
